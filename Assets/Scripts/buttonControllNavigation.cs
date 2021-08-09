cde66c640aeb7dbf47c746b52d6dc2af9ff390ab
geraldo <geraldopireslacerda771@gmail.com>
2021-08-08 22:24:27 -0300

    primeiro commit

---
+++ Assets/Scripts/buttonControllNavigation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonControllNavigation : MonoBehaviour
+{
+
+    public void LoadScene(string name)
+    {
+
+        //Carregar cena de acordo com seu nome
+        SceneManager.LoadScene(name);
+    }
+
+  
+}
