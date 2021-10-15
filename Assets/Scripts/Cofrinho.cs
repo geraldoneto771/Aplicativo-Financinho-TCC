e75c5d70834bcd8f4d11d7b31383381926551df7
geraldo <geraldopireslacerda771@gmail.com>
2021-10-15 13:39:50 -0300

    erro no total do cofrinho

--- Assets/Scripts/Cofrinho.cs
+++ Assets/Scripts/Cofrinho.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Cofrinho : MonoBehaviour
 {
+
+    public InputField inputMoney;
+    private int totalMoney = 0;
+    public Text textTotalCofre;
+    private int money;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,22 @@ public class Cofrinho : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        textTotalCofre.text = "R$" + totalMoney.ToString();
+
+    }
+
+    public void addMoney()
+    {
+
+        money = int.Parse(inputMoney.text);
+        totalMoney += money;
+        
+    }
+
+    public void quebrarCofre()
+    {
+        totalMoney = 0;
         
     }
 }
