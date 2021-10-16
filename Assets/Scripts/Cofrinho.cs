b4256e9ae2f75f28d5f861608027b305c7a2cc45
geraldo <geraldopireslacerda771@gmail.com>
2021-10-16 16:27:03 -0300

    cofrinho finalizado

--- Assets/Scripts/Cofrinho.cs
+++ Assets/Scripts/Cofrinho.cs
@@ -10,11 +10,48 @@ public class Cofrinho : MonoBehaviour
     private double valor;
     public GameObject inputMoney;
     public GameObject textTotalCofre;
+    public GameObject PanelTemCerteza;
+    public GameObject PanelTemCertezaCofre;
+
+
+    public void OpenPanelTemCerteza()
+    {
+        bool isActive = PanelTemCerteza.activeSelf;
+        if (PanelTemCerteza != null)
+        {
+            PanelTemCerteza.SetActive(!isActive);
+
+        }
+        else
+        {
+            PanelTemCerteza.SetActive(isActive);
+
+        }
+
+
+    }
+
+    public void OpenPanelTemCertezaCofre()
+    {
+        bool isActive = PanelTemCertezaCofre.activeSelf;
+        if (PanelTemCertezaCofre != null)
+        {
+            PanelTemCertezaCofre.SetActive(!isActive);
+
+        }
+        else
+        {
+            PanelTemCertezaCofre.SetActive(isActive);
+
+        }
+
+
+    }
 
     public void addMoney()
     {
-       
-        
+        bool isActive = PanelTemCerteza.activeSelf;
+
         try
         {
             if ((double.Parse(inputMoney.GetComponent<Text>().text)) <= 0)
@@ -26,6 +63,18 @@ public class Cofrinho : MonoBehaviour
                 valor += double.Parse(inputMoney.GetComponent<Text>().text);
                 textTotalCofre.GetComponent<Text>().text = "R$" + valor.ToString();
 
+                if (PanelTemCerteza != null)
+                {
+                    PanelTemCerteza.SetActive(!isActive);
+
+                }
+                else
+                {
+                    PanelTemCerteza.SetActive(isActive);
+
+                }
+
+
             }
         }
         catch (Exception e)
@@ -36,9 +85,19 @@ public class Cofrinho : MonoBehaviour
 
     public void quebrarCofre()
     {
-        
+        bool isActive = PanelTemCertezaCofre.activeSelf;
         valor = 0;
         textTotalCofre.GetComponent<Text>().text = "R$" + valor.ToString();
+        if (PanelTemCertezaCofre != null)
+        {
+            PanelTemCertezaCofre.SetActive(!isActive);
+
+        }
+        else
+        {
+            PanelTemCertezaCofre.SetActive(isActive);
+
+        }
 
     }
 
