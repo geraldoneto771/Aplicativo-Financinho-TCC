4570c28f05c83a267f97137782d136a1c7a4ca7c
geraldo <geraldopireslacerda771@gmail.com>
2021-10-20 23:55:26 -0300

    tela inicial do módulo 1 quase finalizada

---
+++ Assets/Scripts/ControleDosBotoesDosNiveis.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDosBotoesDosNiveis : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    
+
+    public void AlterarImagemBolinhaIntroducao1()
+    {
+       
+    }
+
+    public void AlterarImagemBolinhaAventura1()
+    {
+
+    }
+}
